8d3e6833b55f04e9c95f92a146abd98fb41ba54d
hassan-005 <h.hassan5545@gmail.com>
2025-06-10 15:30:27 +0500

    All set with lib.rs also

--- programs/sol_escrow/src/lib.rs
+++ programs/sol_escrow/src/lib.rs
@@ -4,7 +4,7 @@ pub mod state;
 pub mod errors;
 pub mod instructions;
 
-use instructions::make::handler;
+// use instructions::make::handler;
 use instructions::*;
 
 declare_id!("B5RqruQBkjhfvpkKafeRzY4ycwWeoTRwi6o6AxQB9sKu");
@@ -13,13 +13,22 @@ declare_id!("B5RqruQBkjhfvpkKafeRzY4ycwWeoTRwi6o6AxQB9sKu");
 pub mod sol_escrow {
     use super::*;
     pub fn make(ctx: Context<Make>, recieve:u64, amount:u64) -> Result<()> {
-        let seed = &ctx.accounts.escrow.seed;
-        handler(ctx, &seed, recieve, amount)?;
+        let seed = ctx.accounts.escrow.seed;
+        instructions::make::handler(ctx, &seed, recieve, amount)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()>{
+        instructions::take::handler(ctx);
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        instructions::refund::handler(ctx);
         Ok(())
     }
     
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+
 
